101cdc4ccfab64d73f7fc375f6e8fe7ed05f0d24
junge8971 <99283906+junge8971@users.noreply.github.com>
2022-12-06 14:03:44 +0300

    Добавленные лейблы для стартового ввода, реализованна возможность получения значения из стартовых импутов и добавлена схема

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from PySide2.QtWidgets import QWidget,\
     QVBoxLayout,\
     QLineEdit,\
     QPushButton,\
-    QApplication
+    QApplication, \
+    QLabel
+from PySide2.QtGui import QPixmap
 
 
 class MainWindow(QWidget):
@@ -16,26 +18,60 @@ class MainWindow(QWidget):
         self.setWindowTitle("Контрольная работа")
 
         # Входные параметры для вычисления
+        self.label_input_Ube0 = QLabel('UБЭ0, мВ')
         self.input_Ube0 = QLineEdit()  # UБЭ0, мВ
+        self.label_input_Ib0 = QLabel('IБ0, мкА')
         self.input_Ib0 = QLineEdit()  # IБ0, мкА
+        self.label_input_Uke0 = QLabel('UКЭ0, В')
         self.input_Uke0 = QLineEdit()  # UКЭ0, В
+        self.label_input_Ik0 = QLabel('IК0, мА')
         self.input_Ik0 = QLineEdit()  # IК0, мА
+        self.label_input_Ek = QLabel('(EK), В')
         self.input_Ek = QLineEdit()  # Источник питания (EK), В
         self.button_calculate = QPushButton('Расчитать')
+        self.button_calculate.clicked.connect(self.culc)
+
+        # Схема
+        self.img = QPixmap('img/scheme.jpg')
+        self.img_of_scheme = QLabel(self)
+        self.img_of_scheme.setPixmap(self.img)
 
         self.init_UI()
 
     def init_UI(self):
-        h_layout = QHBoxLayout()
+        h0_layout = QHBoxLayout()
+        h1_layout = QHBoxLayout()
+        h2_layout = QHBoxLayout()
         v_layout = QVBoxLayout()
-        v_layout.addLayout(h_layout)
-        h_layout.addWidget(self.input_Ube0)
-        h_layout.addWidget(self.input_Ib0)
-        h_layout.addWidget(self.input_Uke0)
-        h_layout.addWidget(self.input_Ik0)
-        h_layout.addWidget(self.input_Ek)
-        h_layout.addWidget(self.button_calculate)
+        v_layout.addLayout(h0_layout)
+        v_layout.addLayout(h1_layout)
+        v_layout.addLayout(h2_layout)
+
+        h0_layout.addWidget(self.label_input_Ube0)
+        h0_layout.addWidget(self.label_input_Ib0)
+        h0_layout.addWidget(self.label_input_Uke0)
+        h0_layout.addWidget(self.label_input_Ik0)
+        h0_layout.addWidget(self.label_input_Ek)
+
+        h1_layout.addWidget(self.input_Ube0)
+        h1_layout.addWidget(self.input_Ib0)
+        h1_layout.addWidget(self.input_Uke0)
+        h1_layout.addWidget(self.input_Ik0)
+        h1_layout.addWidget(self.input_Ek)
+        h1_layout.addWidget(self.button_calculate)
+
+        h2_layout.addWidget(self.img_of_scheme)
         self.setLayout(v_layout)
+
+    def culc(self):
+        Ube0 = self.input_Ube0.text()
+        Ib0 = self.input_Ib0.text()
+        Uke0 = self.input_Uke0.text()
+        Ik0 = self.input_Ik0.text()
+        Ek = self.input_Ek.text()
+        if Ube0 and Ib0 and Uke0 and Ik0 and Ek:
+            print(1)
+
 
 class HorizontalResistor:
     def __init__(self, x, y, r_value):
